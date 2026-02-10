c8bc68b0c93b342e472d2fce861cb120530d9017
Abdullah Meshal <135350887+abdullahMesha@users.noreply.github.com>
2026-02-10 17:39:14 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ const projects = [
                 <li>Site management tools including RF data entry, equipment tracking, and comparison utilities</li>
             </ul>`,
         tech: ['.NET', 'C#', 'MySQL', 'Web UI'],
-        images: [ 'assets/img/ran-helpdesk-compare.png','assets/img/ran-helpdesk.png],
+        images: [ 'assets/img/ran-helpdesk-compare.png','assets/img/ran-helpdesk.png'],
         gradient: 'linear-gradient(135deg, #dc2626, #9333ea)',
         icon: 'M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z'
     },
